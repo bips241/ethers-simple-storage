262f98dd7f33165b7e4200380bc6b4d463f47dbf
biplab <biplabmal626@gmail.com>
2023-11-16 02:55:28 +0530

    first commit

--- deploy.js
+++ deploy.js
@@ -1,5 +1,23 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function main() {
-  console.log("hi");
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://172.22.160.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "0x965fe1a274ff58d470415ac0904f77842dbf550f907a015a469ee97bdf3b8392",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
